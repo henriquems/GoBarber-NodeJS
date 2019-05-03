e8b1387fe4c974ae82158af646673e172ce9e69a
FELUMA\henrique.simoes <henriquems@gmail.com>
2019-05-03 16:01:06 -0300

    Curso NodeJS modulo 2 bootcamp

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mssql',
+  host: '192.168.0.26',
+  username: 'sa',
+  password: '@sql08#$',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
